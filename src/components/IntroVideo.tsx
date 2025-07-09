e9784e8c49cf7a2e77c83806d58bdca2d32bc2fd
shanpriya2005 <shanananthi22@gmail.com>
2025-07-09 10:03:16 +0530

    Update IntroVideo.tsx

--- src/components/IntroVideo.tsx
+++ src/components/IntroVideo.tsx
@@ -13,7 +13,7 @@ const IntroVideo = ({ onFinish }: { onFinish: () => void }) => {
   return (
     <div className="fixed inset-0 bg-black flex items-center justify-center z-50">
       <video 
-        src="/cholanadu intro.mp4" // Your video path
+        src="https://res.cloudinary.com/degljuqjv/video/upload/v1752035543/cholanadu_inro_mp4_cqpwxb.mp4" // Your video path
         autoPlay 
         muted 
         className="w-full h-full object-cover"
